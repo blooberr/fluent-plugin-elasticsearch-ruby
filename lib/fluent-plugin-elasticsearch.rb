af250bc3a76b9590f1cba646ae50e7be9176de52
blooberr <blooberr@gmail.com>
2012-12-20 00:32:04 -0800

    new repo version

---
+++ lib/fluent-plugin-elasticsearch.rb
@@ -0,0 +1,9 @@
+require "fluent-plugin-elasticsearch/version"
+
+module Fluent
+  module Plugin
+    module Elasticsearch
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/fluent-plugin-elasticsearch/version.rb
@@ -0,0 +1,7 @@
+module Fluent
+  module Plugin
+    module Elasticsearch
+      VERSION = "0.0.1"
+    end
+  end
+end
